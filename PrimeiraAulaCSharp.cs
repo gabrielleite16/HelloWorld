0c5771a19e58bebe4b3028ec19281a692d4c9b93
gabrielleite16 <36733599+gabrielleite16@users.noreply.github.com>
2018-08-21 20:24:51 -0300

    Add files via upload

---
+++ PrimeiraAulaCSharp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        // Linha única usamos duas barras;
+        /*
+         para comentários
+         com mais de uma linha;
+        */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.WriteLine("What is your football team?");
+            var name2 = Console.ReadLine();
+            Console.WriteLine($"\nWow, {name2}, is the best football team in Brazil!");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
+        } // Fim do método Main
+    } // Fim da classe Program
+} // Fim do namespace HelloWorld
