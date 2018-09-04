2f0494de30f034a9f1d5d3cca0d5f2d533ed98d4
gabrielleite16 <36733599+gabrielleite16@users.noreply.github.com>
2018-09-04 20:32:01 -0300

    Add files via upload

---
+++ Pessoa.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Aula04092018
+{
+    class Pessoa
+    {
+        // Atributos
+        private string nome;
+        private int idade;
+        private double altura;
+        private double peso;
+        private string cpf;
+
+        // Método Construtor
+        public Pessoa()
+        {
+            this.nome = "";
+            this.idade = 0;
+            this.altura = 0.0;
+            this.peso = 0.0;
+            this.cpf = "";
+        }
+
+        public Pessoa(string cpf, string nome)
+        {
+            this.cpf = cpf;
+            this.nome = nome;
+        }
+
+        //Métodos
+        public string NomeCPF()
+        {
+            return this.nome + " - " + this.cpf;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula04092018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+            Pessoa pessoa2 = new Pessoa("14523750629", "Gabriel Leite");
+
+            Console.WriteLine(pessoa2.NomeCPF());
+
+            Console.WriteLine("\n\n\nPressione qualquer tecla para sair...");
+            Console.ReadKey(true);
+
+        }
+    }
+}
